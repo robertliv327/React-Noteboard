389f1c5c028523a14b5a3e41edda549481f86e7b
Robert Livaudais <robert.h.livaudais.19@dartmouth.edu>
2018-04-12 14:15:03 -0400

    push before class

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const env = process.env.NODE_ENV || 'development';
+// set to 'production' or 'development' in your env
+
+module.exports = {
+  mode: env,
+  entry: ['./src'], // this is where our app lives
+  devtool: 'source-map', // this enables debugging with source in chrome devtools
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: './index.html',
+    }),
+  ],
+};
