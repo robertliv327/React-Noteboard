9b01e4f7e008438d52e12d9241a0d22e73060349
Robert Livaudais <robert.h.livaudais.19@dartmouth.edu>
2018-04-13 10:04:23 -0400

    new starter with react

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
-import $ from 'jquery';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './style.scss';
 
-let numSeconds = 0;
-setInterval(() => {
-  numSeconds += 1;
-  $('#main').html(`You have been on this page for ${numSeconds} seconds.`);
-}, 1000);
+const App = () => {
+  return <div className="test">All the React are belong to us!</div>;
+};
+
+ReactDOM.render(<App />, document.getElementById('main'));
+
+// let numSeconds = 0;
+// setInterval(() => {
+//   numSeconds += 1;
+//   $('#main').html(`You have been on this page for ${numSeconds} seconds.`);
+// }, 1000);
